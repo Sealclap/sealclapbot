0be0f7a37a6df27e9642c7e4649382d1a9826b68
Dale Bowles <dalebowles93@gmail.com>
2021-12-12 15:05:16 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+    connection: {
+        reconnect: true
+    },
+    options: { debug: true },
+	identity: {
+		username: process.env.TWITCH_USERNAME,
+		password: process.env.TWITCH_TOKEN
+	},
+	channels: [ 'sealclap' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// "Alca: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+
+    if(message.toLowerCase() === 'hello' || message.toLowerCase() === 'hey' || message.toLowerCase() === 'hi') {
+        client.say(channel, `Hey there, @${tags.username}!`)
+    };
+
+    if(tags.username === 'buttsbot' && message.toLowerCase() !== ':d') {
+        client.say(channel, 'Buttsbot yes');
+    };
+
+    if(self || !message.startsWith('!')) return;
+
+	const args = message.slice(1).split(' ');
+	const command = args.shift().toLowerCase();
+
+	if(command === 'echo') {
+		client.say(channel, `@${tags.username}, you said: "${args.join(' ')}"`);
+	};
+
+    if(command === 'halnett') {
+        client.say(channel, `@${tags.username}, you should know that @ElementalityNJ hates this sound!`);
+    };
+});
